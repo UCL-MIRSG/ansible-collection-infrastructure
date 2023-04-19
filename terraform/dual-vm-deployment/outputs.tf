3720dbc73c5045c5f18809b07631aebf8ed219ff
Daniel Matthews <d.matthews@ucl.ac.uk>
2023-04-19 12:48:33 +0100

    pre-commit formatting fixes.

--- terraform/dual-vm-deployment/outputs.tf
+++ terraform/dual-vm-deployment/outputs.tf
@@ -2,8 +2,8 @@
 output "terraform_var_host_list" {
   value = [
     {
-        "terraform_var_ip" : local.web_ssh_cluster_ip,
-        "terraform_var_port": local.web_ssh_node_port,
+      "terraform_var_ip" : local.web_ssh_cluster_ip,
+      "terraform_var_port" : local.web_ssh_node_port,
     }
   ]
 }
